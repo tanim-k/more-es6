78f8db515e1b0218e26d726916be5de4074847f9
tanim-k <tanimkhan877@gmail.com>
2022-02-26 00:22:55 +0600

    destucturing an boject

--- destructuring.js
+++ destructuring.js
@@ -14,3 +14,31 @@ console.log(phone, id);
 console.log(phone, dress);
 console.log(phone);
 console.log(phone, address);
+
+// use destructuring in complex case 
+const company = {
+    mane: 'gp',
+    ceo: {
+        id:1, 
+        name:'ajmol', 
+        food: 'fuchka'
+    },
+    web: {
+        work:'website development',
+        employee: 22, 
+        framework: 'react', 
+        tech:{ 
+            first:'html',
+            second:'css', 
+            third: 'js'
+        }
+    },
+}
+// const work = company.web.work;
+// const frameWork = company.web.framework;
+// console.log(frameWork)
+// shortcut 
+const {work, framework} = company.web;
+const {food} = company.ceo;
+const {second, third} = company.web.tech;
+console.log(work, framework, food, second, third)
