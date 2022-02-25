5a822bde90bb63f0bfd3920f8995ddc106159f7c
tanim-k <tanimkhan877@gmail.com>
2022-02-25 23:27:30 +0600

    let const

--- recap.js
+++ recap.js
@@ -1,3 +1,13 @@
 const hubby = 'omar sani';
 let phone = 'hhrte44';
 phone = 'sam34 hqpo';
+// template strings 
+const myNotes = `i am nojnu of ${hubby}. i dont have a laili. i have a ${phone}`
+console.log(myNotes);
+// default parameters
+function maxNumber(array = []) {
+    const max = Math.max(...array);
+    return max;
+}  
+const biggest = maxNumber();
+console.log(biggest); 
